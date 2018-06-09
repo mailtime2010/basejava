310f21be6ef2272656004b3cd6081c75a74d9af3
Mailtime <mailtime2010@gmail.com>
2018-06-10 02:03:15 +0300

    HMW1.To updated save()/delete()/get(). To wrote cheks.

--- src/com/basejava/testjava/ArrayStorage.java
+++ src/com/basejava/testjava/ArrayStorage.java
@@ -12,25 +12,30 @@ public class ArrayStorage {
         if(countSize < 10000) {
             storage[countSize] = resume;
             countSize++;
-        }
+        } else {System.out.println("Вы не можете добавить резюме. Массив полностью заполненен.");}
     }
 
     public Resume get(String name) {
-        for(int i = 0; i < countSize; i++) {
-            if(storage[i].name.equals(name)) { return storage[i]; }
+        if(countSize != 0) {
+            for(int i = 0; i < countSize; i++) {
+                if(storage[i].name.equals(name)) { return storage[i]; }
+            }
         }
+        else System.out.println("Вы не можете получить резюме.Массив резюме пустой.");
         return null;
     }
 
     public void delete(Resume resume) {
-        for(int i = 0; i < countSize; i++) {
-            if(storage[i].equals(resume)) {
-                storage[i] = storage[countSize - 1];
-                storage[countSize-1] = null;
-                countSize--;
-                break;
-            }
-        }
+       if(countSize !=0) {
+           for(int i = 0; i < countSize; i++) {
+               if(storage[i].equals(resume)) {
+                   storage[i] = storage[countSize - 1];
+                   storage[countSize - 1] = null;
+                   countSize--;
+                   break;
+               }
+           }
+       } else System.out.println("Вы не можете удалить резюме.Массив резюме пустой.");
     }
 
     public int size() {

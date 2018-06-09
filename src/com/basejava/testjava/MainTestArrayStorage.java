7e2db8b50593f7574b9c294b5a614973ebd20886
Mailtime <mailtime2010@gmail.com>
2018-06-10 02:04:14 +0300

    HMW1. To update the interface.

--- src/com/basejava/testjava/MainTestArrayStorage.java
+++ src/com/basejava/testjava/MainTestArrayStorage.java
@@ -11,7 +11,6 @@ public class MainTestArrayStorage {
         Resume resume2 = new Resume("2");
         Resume resume3 = new Resume("3");
 //****************************
-        System.out.println("Максим, тебе нужно отдохнуть, а то от кода ничего не останется ))");
         System.out.println();
         System.out.print("SAVE(): ");
         storage.save(resume);
@@ -38,26 +37,46 @@ public class MainTestArrayStorage {
         storage.save(resume2);
         storage.save(resume3);
         System.out.println("Добавляю в массив " + storage.size() + " резюме...");
-        System.out.println("Максим,это испытание огнём, хоть резюме и добавлены в массив.)");
-        System.out.println("В массиве снова: " + storage.size() + " резюме.");
+        System.out.println("В массиве: " + storage.size() + " резюме.");
         Resume[] threeStor = storage.getAll();
         System.out.println("GET_All[элементы массива]: " + Arrays.toString(threeStor));
         System.out.println();
 //*****************************
         System.out.print("GET(): ");
-        System.out.println("Получение резюме " + storage.get("2") + "...");
-        System.out.println("Мы получили ваше резюме: " + storage.get("2") + ", хотя уже не уверены нужно или оно нам)");
+        System.out.println("Получение резюме " + resume2 + "...");
+        System.out.println("Получено резюме: " + storage.get("2"));
         System.out.println();
 //*****************************
         System.out.print("DELETE(): ");
         System.out.println("Удаление резюме: " + resume2 + "...");
-        storage.delete(resume1);
-        System.out.println("К сожалению, Ваше резюме " + resume2 + ", удалено из массива.");
+        storage.delete(resume2);
         System.out.println("В массиве: " + storage.size() + " резюме.");
 //*****************************
         Resume[] fourStor = storage.getAll();
         System.out.println("GET_ALL[элементы массива]: " + Arrays.toString(fourStor));
         System.out.println();
-        System.out.println("А как хорошо же всё начиналось :/" );
+        //*****************************
+        System.out.print("CLEAR(): ");
+        System.out.println("Очистка массива...");
+        storage.clear();
+        System.out.println("В массиве: " + storage.size() + " резюме.");
+        Resume[] fifthStor = storage.getAll();
+        System.out.println("GET_All[элементы массива]: " + Arrays.toString(fifthStor));
+        System.out.println();
+//*****************************
+        System.out.print("GET(): ");
+        System.out.println("Получение резюме " + resume2 + "...");
+        System.out.println("Получено резюме: " + storage.get("2"));
+        System.out.println("GET_All[элементы массива]: " + Arrays.toString(fifthStor));
+        System.out.println();
+//*****************************
+        System.out.print("DELETE(): ");
+        System.out.println("Удаление резюме: " + resume2 + "...");
+        storage.delete(resume2);
+        System.out.println("В массиве: " + storage.size() + " резюме.");
+        System.out.println("GET_All[элементы массива]: " + Arrays.toString(fifthStor));
+        System.out.println();
+//*****************************
+        System.out.println(" 1:57 рм а я пишу код...иду спать.." );
     }
 }

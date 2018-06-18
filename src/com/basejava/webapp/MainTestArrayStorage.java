4bcfd42c815bb51d7e83311dfa81d9dcc73b4202
Mailtime <mailtime2010@gmail.com>
2018-06-18 17:34:32 +0300

    HMW4. Object Resume was changed by new constructors. The method setUuid() was deleted.

--- src/com/basejava/webapp/MainTestArrayStorage.java
+++ src/com/basejava/webapp/MainTestArrayStorage.java
@@ -2,45 +2,44 @@ package com.basejava.webapp;
 
 
 import com.basejava.webapp.model.Resume;
-import com.basejava.webapp.storage.ArrayStorage;
+import com.basejava.webapp.storage.SortedArrayStorage;
 import com.basejava.webapp.storage.Storage;
 
 /**
  * Test com.basejava.webapp.storage;
  */
 public class MainTestArrayStorage {
-    private static final Storage ARRAY_STORAGE = new ArrayStorage();
+    private static final Storage ARRAY_STORAGE = new SortedArrayStorage();
 
     public static void main(String[] args) {
 
-        Resume r1 = new Resume();
-        r1.setUuid("uuid2");
-        Resume r2 = new Resume();
-        r2.setUuid("uuid3");
-        Resume r3 = new Resume();
-        r3.setUuid("uuid1");
-        Resume r4 = new Resume();
-        r4.setUuid("uuid4");
-        Resume r5 = new Resume();
-        r5.setUuid("uuid5");
+        Resume r1 = new Resume("uuid1");
+        Resume r2 = new Resume("uuid2");
+        Resume r3 = new Resume("uuid4");
+        Resume r4 = new Resume("uuid3");
+        Resume r5 = new Resume("uuid5");
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
         ARRAY_STORAGE.save(r4);
-        ARRAY_STORAGE.save(r5);
+        printAll();
         System.out.println("Size: " + ARRAY_STORAGE.size());
         System.out.println();
+        ARRAY_STORAGE.save(r2);
+        printAll();
+        System.out.println();
 
-        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
+        System.out.println("Get uuid1: " + ARRAY_STORAGE.get(r1.getUuid()));
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
         System.out.println();
 
         ARRAY_STORAGE.update(r2);
-        ARRAY_STORAGE.update(r4);
-        printAll();
+        ARRAY_STORAGE.update(r5);
         System.out.println();
+        printAll();
         ARRAY_STORAGE.delete(r1.getUuid());
         printAll();
+        ARRAY_STORAGE.delete(r5.getUuid());
         ARRAY_STORAGE.clear();
         printAll();
 
@@ -48,10 +47,12 @@ public class MainTestArrayStorage {
     }
 
     static void printAll() {
-        System.out.println("\nGet All: ");
+        System.out.print("GET ALL: ");
+        System.out.print("[ ");
         for(Resume r : ARRAY_STORAGE.getAll()) {
             System.out.print(r + " ");
         }
+        System.out.print("]");
         System.out.println();
     }
 }

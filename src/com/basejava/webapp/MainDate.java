9f8e432d49edb25e054b4e3df5c2a4bc25630bdc
Mailtime <mailtime2010@gmail.com>
2018-07-06 19:59:59 +0300

    HMW8. MainDate was added.

--- src/com/basejava/webapp/MainDate.java
+++ src/com/basejava/webapp/MainDate.java
@@ -1,5 +1,6 @@
 package com.basejava.webapp;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -7,6 +8,7 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 
 public class MainDate {
@@ -24,14 +26,43 @@ public class MainDate {
         System.out.println(ld);
         LocalTime lt = LocalTime.now();
         System.out.println(lt);
-        LocalDateTime ltd = LocalDateTime.of(ld,lt);
+        LocalDateTime ltd = LocalDateTime.of(ld, lt);
         System.out.println(ltd);
 
+       /* ltd = LocalDateTime.parse("June 21, 2018 12:01 АМ",
+                DateTimeFormatter.ofPattern("ММММ dd',' уууу hh':'mm а"));
+        System.out.println(ltd.format(DateTimeFormatter.ofPattern("ММММ d',' уууу h':'mm а")));
+       */
         SimpleDateFormat sdf = new SimpleDateFormat("<YY><MM><dd>");
         System.out.println(sdf.format(date));
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("YY/MM/dd");
         System.out.println(dtf.format(ltd));
+
+        DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT, Locale.JAPAN);
+        System.out.println("Япония : " + df.format(date));
+        df = DateFormat.getDateInstance(DateFormat.LONG, Locale.JAPAN);
+        System.out.println("Япония : " + df.format(date));
+
+        df = DateFormat.getDateInstance(DateFormat.SHORT, Locale.CHINA);
+        System.out.println("Китай : " + df.format(date));
+        df = DateFormat.getDateInstance(DateFormat.FULL, Locale.CHINA);
+        System.out.println("Китай : " + df.format(date));
+
+        df = DateFormat.getDateInstance(DateFormat.SHORT, Locale.US);
+        System.out.println("США : " + df.format(date));
+        df = DateFormat.getDateInstance(DateFormat.FULL, Locale.US);
+        System.out.println("США : " + df.format(date));
+
+        df = DateFormat.getDateInstance(DateFormat.SHORT, Locale.getDefault());
+        System.out.println("Украина : " + df.format(date));
+        df = DateFormat.getDateInstance(DateFormat.FULL, Locale.getDefault());
+        System.out.println("Украина : " + df.format(date));
+
+        SimpleDateFormat simpledf = new SimpleDateFormat("hh:mm:ss");
+        System.out.println(simpledf.format(date));
+        simpledf = new SimpleDateFormat("dd MMM yyyy hh:mm:ss zzz");
+        System.out.println(simpledf.format(date));
+        simpledf = new SimpleDateFormat("E d M y h:m:s z");
+        System.out.println(simpledf.format(date));
     }
-
-
 }

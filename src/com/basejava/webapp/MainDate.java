af11d02c14c60aef32b9954cd9920fd20a627a8c
Mailtime <mailtime2010@gmail.com>
2018-07-06 15:18:11 +0300

    HMW8. MainDate.java was added.

---
+++ src/com/basejava/webapp/MainDate.java
@@ -0,0 +1,37 @@
+package com.basejava.webapp;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class MainDate {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        Date date = new Date();
+        System.out.println(date);
+        System.out.println(System.currentTimeMillis() - start);
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar);
+        calendar.setTimeZone(TimeZone.getTimeZone("America/Los_Angeles"));
+        System.out.println(calendar.getTime());
+
+        LocalDate ld = LocalDate.now();
+        System.out.println(ld);
+        LocalTime lt = LocalTime.now();
+        System.out.println(lt);
+        LocalDateTime ltd = LocalDateTime.of(ld,lt);
+        System.out.println(ltd);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("<YY><MM><dd>");
+        System.out.println(sdf.format(date));
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("YY/MM/dd");
+        System.out.println(dtf.format(ltd));
+    }
+
+
+}

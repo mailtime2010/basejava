0ee930ca02a4bff653370801238af7e673c26fdb
Mailtime <mailtime2010@gmail.com>
2018-07-07 18:57:26 +0300

    HMW8. MainScanner was changed.

--- src/com/basejava/webapp/MainScanner.java
+++ src/com/basejava/webapp/MainScanner.java
@@ -17,21 +17,21 @@ public class MainScanner {
         fout.write("2 3.4 5 6 7.4 9.1 1 0.5 готово");
         fout.close();
         FileReader fin = new FileReader("F:/testAAA.txt");
-        Scanner src = new Scanner(fin);
-        while(src.hasNext()) {
-            if(src.hasNextDouble()) {
-                sum += src.nextDouble();
-                count++;
-            } else {
-                String str = src.next();
-                if(str.equals("готово")) break;
-                else {
-                    System.out.println("Ошибка формата файла.");
-                    return;
+        try(Scanner src = new Scanner(fin)) {
+            while(src.hasNext()) {
+                if(src.hasNextDouble()) {
+                    sum += src.nextDouble();
+                    count++;
+                } else {
+                    String str = src.next();
+                    if("готово".equals(str)) break;
+                    else {
+                        System.out.println("Ошибка формата файла.");
+                        return;
+                    }
                 }
             }
         }
-        src.close();
         System.out.println("Среднее равно " + sum / count);
     }
 }

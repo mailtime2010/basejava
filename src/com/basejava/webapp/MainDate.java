b300883e17a9e9ab0633435da7328b07bcd1f625
Mailtime <mailtime2010@gmail.com>
2018-07-07 16:15:20 +0300

    HMW8. MainDate was changed.

--- src/com/basejava/webapp/MainDate.java
+++ src/com/basejava/webapp/MainDate.java
@@ -2,9 +2,7 @@ package com.basejava.webapp;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
@@ -12,15 +10,20 @@ import java.util.Locale;
 import java.util.TimeZone;
 
 public class MainDate {
+
+    private static final String DATE_FORMAT = "dd-M-yyyy hh:mm:ss a";
+
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
         Date date = new Date();
-        System.out.println(date);
-        System.out.println(System.currentTimeMillis() - start);
+        System.out.println(date);//the same
+        System.out.println("Differ: " + (System.currentTimeMillis() - start));
+
         Calendar calendar = Calendar.getInstance();
-        System.out.println(calendar);
+        //System.out.println(calendar);
         calendar.setTimeZone(TimeZone.getTimeZone("America/Los_Angeles"));
-        System.out.println(calendar.getTime());
+        System.out.println(calendar.getTime());//the same
+        System.out.println();
 
         LocalDate ld = LocalDate.now();
         System.out.println(ld);
@@ -28,15 +31,27 @@ public class MainDate {
         System.out.println(lt);
         LocalDateTime ltd = LocalDateTime.of(ld, lt);
         System.out.println(ltd);
+        System.out.println();
 
-       /* ltd = LocalDateTime.parse("June 21, 2018 12:01 АМ",
+        SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
+        TimeZone tz = TimeZone.getDefault();
+        System.out.println("TimeZone : " + tz);
+        System.out.println("TimeZone : " + tz.getID() + " - " + tz.getDisplayName());
+        // From TimeZone Asia/Singapore
+        System.out.println("Date (Ukraine) : " + formatter.format(date));
+        System.out.println();
+
+      /* String dateInString = "22-1-2015 10:15:55 AM";
+        LocalDateTime ldt = LocalDateTime.parse(dateInString, DateTimeFormatter.ofPattern("dd-M-yyyy hh:mm:ss a"));
+        ltd = LocalDateTime.parse("June 21, 2018 12:01 АМ",
                 DateTimeFormatter.ofPattern("ММММ dd',' уууу hh':'mm а"));
         System.out.println(ltd.format(DateTimeFormatter.ofPattern("ММММ d',' уууу h':'mm а")));
-       */
+      */
         SimpleDateFormat sdf = new SimpleDateFormat("<YY><MM><dd>");
         System.out.println(sdf.format(date));
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("YY/MM/dd");
         System.out.println(dtf.format(ltd));
+        System.out.println();
 
         DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT, Locale.JAPAN);
         System.out.println("Япония : " + df.format(date));
@@ -57,6 +72,7 @@ public class MainDate {
         System.out.println("Украина : " + df.format(date));
         df = DateFormat.getDateInstance(DateFormat.FULL, Locale.getDefault());
         System.out.println("Украина : " + df.format(date));
+        System.out.println();
 
         SimpleDateFormat simpledf = new SimpleDateFormat("hh:mm:ss");
         System.out.println(simpledf.format(date));
@@ -64,5 +80,26 @@ public class MainDate {
         System.out.println(simpledf.format(date));
         simpledf = new SimpleDateFormat("E d M y h:m:s z");
         System.out.println(simpledf.format(date));
+        System.out.println();
+
+        sdf = new SimpleDateFormat("yyyy MMMM dd HH:mm:ss");
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, 2002);
+        cal.set(Calendar.MONTH, Calendar.AUGUST);
+        cal.set(Calendar.DAY_OF_MONTH, 25);
+        cal.set(Calendar.MINUTE, 00);
+        cal.set(Calendar.SECOND, 00);
+        System.out.println("set date: " + sdf.format(cal.getTime()));
+        cal.set(Calendar.MONTH, Calendar.FEBRUARY);
+        System.out.println("month changed : " + sdf.format(cal.getTime()));
+        cal.set(Calendar.DAY_OF_MONTH, 25);
+        System.out.println("day changed : " + sdf.format(cal.getTime()));
+        // if day 31 or 30 and month february be careful
+        cal.add(Calendar.SECOND, 75);
+        System.out.println("add 75 ms: " + sdf.format(cal.getTime()));
+        cal.add(Calendar.MONTH, 1);
+        System.out.println("add 1 month: " + sdf.format(cal.getTime()));
+
+
     }
 }

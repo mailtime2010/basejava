3b2ba424bf5022751f75287718f66b26033d1782
Mailtime <mailtime2010@gmail.com>
2018-07-09 19:00:08 +0300

    HMW8. it was changed.

--- src/com/basejava/webapp/model/ListSection.java
+++ src/com/basejava/webapp/model/ListSection.java
@@ -1,10 +1,15 @@
 package com.basejava.webapp.model;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 public class ListSection extends Section {
     private final List<String> items;
+
+    public ListSection(String... items) {
+        this(Arrays.asList(items));
+    }
 
     public ListSection(List<String> items) {
         Objects.requireNonNull(items, "items must not be null");
@@ -22,8 +27,8 @@ public class ListSection extends Section {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
 
         ListSection that = (ListSection) o;
 

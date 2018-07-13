d4226047c52f79271e8646194ee8708e453468a0
Mailtime <mailtime2010@gmail.com>
2018-07-13 19:34:31 +0300

    HMW9.

--- test/com/basejava/webapp/storage/AbstractStorageTest.java
+++ test/com/basejava/webapp/storage/AbstractStorageTest.java
@@ -16,6 +16,7 @@ import static org.junit.Assert.assertTrue;
 
 public abstract class AbstractStorageTest {
     protected static final File STORAGE_DIR = new File("F:/webapp");
+
     protected Storage storage;
 
     private static final String UUID_1 = "uuid1";
@@ -40,11 +41,23 @@ public abstract class AbstractStorageTest {
         R1.addSection(SectionType.PERSONAL, new TextSection("Personal data"));
         R1.addSection(SectionType.ACHIEVEMENT, new ListSection("Achivment11", "Achivment12", "Achivment13"));
         R1.addSection(SectionType.QUALIFICATIONS, new ListSection("Java", "SQL", "JavaScript"));
-        R1.addSection(SectionType.EXPERIENCE, new OrganizationSection(new Organization("Organization11", "http://Organization11.ru", new Organization.Position(2005, Month.JANUARY, "position1", "content1"), new Organization.Position(2001, Month.MARCH, 2005, Month.JANUARY, "position2", "content2"))));
-        R1.addSection(SectionType.EDUCATION, new OrganizationSection(new Organization("Institute", null, new Organization.Position(1996, Month.JANUARY, 2000, Month.DECEMBER, "aspirant", null), new Organization.Position(2001, Month.MARCH, 2005, Month.JANUARY, "student", "IT facultet")), new Organization("Organization12", "http://Organization12.ru")));
+        R1.addSection(SectionType.EXPERIENCE,
+                new OrganizationSection(
+                        new Organization("Organization11", "http://Organization11.ru",
+                                new Organization.Position(2005, Month.JANUARY, "position1", "content1"),
+                                new Organization.Position(2001, Month.MARCH, 2005, Month.JANUARY, "position2", "content2"))));
+        R1.addSection(SectionType.EDUCATION,
+                new OrganizationSection(
+                        new Organization("Institute", null,
+                                new Organization.Position(1996, Month.JANUARY, 2000, Month.DECEMBER, "aspirant", null),
+                                new Organization.Position(2001, Month.MARCH, 2005, Month.JANUARY, "student", "IT facultet")),
+                        new Organization("Organization12", "http://Organization12.ru")));
         R2.addContact(ContactType.SKYPE, "skype2");
         R2.addContact(ContactType.PHONE, "22222");
-        R1.addSection(SectionType.EXPERIENCE, new OrganizationSection(new Organization("Organization2", "http://Organization2.ru", new Organization.Position(2015, Month.JANUARY, "position1", "content1"))));
+        R1.addSection(SectionType.EXPERIENCE,
+                new OrganizationSection(
+                        new Organization("Organization2", "http://Organization2.ru",
+                                new Organization.Position(2015, Month.JANUARY, "position1", "content1"))));
     }
 
     protected AbstractStorageTest(Storage storage) {

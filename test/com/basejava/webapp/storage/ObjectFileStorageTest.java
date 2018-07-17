f8d1a94540e0accb10992f5bcd9be7d66220b8c5
Mailtime <mailtime2010@gmail.com>
2018-07-17 22:11:17 +0300

    HMW9. IO

--- test/com/basejava/webapp/storage/ObjectFileStorageTest.java
+++ test/com/basejava/webapp/storage/ObjectFileStorageTest.java
@@ -5,6 +5,6 @@ import com.basejava.webapp.storage.serializer.ObjectStreamSerializer;
 public class ObjectFileStorageTest extends AbstractStorageTest {
 
     public ObjectFileStorageTest() {
-        super(new FileStorage(STORAGE_DIR, new ObjectStreamSerializer()));
+        super(new FileStorage(STORAGE_DIR.getAbsoluteFile(), new ObjectStreamSerializer()));
     }
 }

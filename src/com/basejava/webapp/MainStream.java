30143946d0a84776baaa024e0a85fad0dc5be6b6
Mailtime <mailtime2010@gmail.com>
2018-07-19 14:43:52 +0300

    HMW9. Stream

--- src/com/basejava/webapp/MainStream.java
+++ src/com/basejava/webapp/MainStream.java
@@ -18,13 +18,19 @@ public class MainStream {
             e.printStackTrace();
         }
         int sum = Stream.of(10, 2, 3, 4, 5)
-                .peek(e -> System.out.println("Taking integer: " + e))// take 10.2.3//4.5
+                .peek(e -> System.out.println("Taking %2 integer: " + e))// take to( 10,2)3// to (4)5
                 .filter(n -> n % 2 == 1)//filter 3//5
-                .peek(e -> System.out.println("Filtered integer: " + e))// print 3//5
+                .peek(e -> System.out.println("Filtered %2 ==1 integer: " + e))// print 3//5
                 //multiply after filer 3*3// return to take// multyply after filter 5*5
                 .map(n -> n * n).peek(e -> System.out.println("Mapped integer: " + e))
                 .reduce(0, Integer::sum); // 9+25=34
         System.out.println("Sum = " + sum);//34
+        Stream.of(1,2,3,4,5)
+                .peek(e -> System.out.println("Taking %5 to: "+e))
+                .filter(n ->n%5 ==0)
+                .peek(e ->System.out.println("f: "+e))
+                .map(n->n*n).peek(e-> System.out.println("map: "+e))
+                .reduce(0,Integer::sum);
 
     }
 }

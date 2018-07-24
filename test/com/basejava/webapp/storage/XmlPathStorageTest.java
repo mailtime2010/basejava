5f937fcbd3d3c2cbea4db03de43f5dd50f16787d
Mailtime <mailtime2010@gmail.com>
2018-07-24 13:40:03 +0300

    HMW10. XML

--- test/com/basejava/webapp/storage/XmlPathStorageTest.java
+++ test/com/basejava/webapp/storage/XmlPathStorageTest.java
@@ -2,9 +2,9 @@ package com.basejava.webapp.storage;
 
 import com.basejava.webapp.storage.serializer.XmlStreamSerializer;
 
-public class XmlPathStorageTest extends AbstractStorageTest{
+public class XmlPathStorageTest extends AbstractStorageTest {
 
-    protected XmlPathStorageTest() {
-        super(new PathStorage(STORAGE_DIR.getAbsolutePath(),new XmlStreamSerializer()));
+    public XmlPathStorageTest() {
+        super(new PathStorage(STORAGE_DIR.getAbsolutePath(), new XmlStreamSerializer()));
     }
 }

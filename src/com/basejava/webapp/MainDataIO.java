36693fc6e9d56ccca9325fdcaf613d3d7050c822
Mailtime <mailtime2010@gmail.com>
2018-07-26 13:36:34 +0300

    HMW9. change instance

--- src/com/basejava/webapp/MainDataIO.java
+++ src/com/basejava/webapp/MainDataIO.java
@@ -5,25 +5,24 @@ import java.io.*;
 public class MainDataIO {
     public static void main(String[] args) {
         //write data to file primitive type
-        try(DataOutputStream dout = new DataOutputStream(new FileOutputStream("f:\\Numbers.dat"))) {
-            dout.writeDouble(98.6);
-            dout.writeInt(1000);
-            dout.writeBoolean(true);
+        try(DataOutputStream dos = new DataOutputStream(new FileOutputStream("f:\\Numbers.dat"))) {
+            dos.writeDouble(98.6);
+            dos.writeInt(1000);
+            dos.writeBoolean(true);
         } catch(FileNotFoundException e) {
-            System.out.println("Can't open file in");
+            System.out.println("Can't open file output");
             return;
         } catch(IOException e) {
             System.out.println("Mistake in-out" + e);
         }
         //read primitive type data from file
-        try(DataInputStream din = new DataInputStream(new FileInputStream("f:\\Numbers.dat"))) {
-            double d = din.readDouble();
-            int i = din.readInt();
-            boolean b = din.readBoolean();
+        try(DataInputStream dis = new DataInputStream(new FileInputStream("f:\\Numbers.dat"))) {
+            double d = dis.readDouble();
+            int i = dis.readInt();
+            boolean b = dis.readBoolean();
             System.out.println("Read meanings: " + d + " " + i + " " + b);
-            return;
         } catch(FileNotFoundException e) {
-            System.out.println("Can't open file in");
+            System.out.println("Can't open file input");
         } catch(IOException e) {
             System.out.println("Mistake in-out" + e);
         }

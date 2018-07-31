322f7099423fc43961fa3de17f8c2d8a0c2f153a
Mailtime <mailtime2010@gmail.com>
2018-07-31 13:25:19 +0300

    clear

--- src/com/basejava/webapp/storage/serializer/StreamSerializer.java
+++ src/com/basejava/webapp/storage/serializer/StreamSerializer.java
@@ -7,7 +7,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public interface StreamSerializer {
-    void doWrite(Resume r, OutputStream os) throws IOException;
 
+    void doWrite(Resume r, OutputStream os) throws IOException;
     Resume doRead(InputStream is) throws IOException;
 }

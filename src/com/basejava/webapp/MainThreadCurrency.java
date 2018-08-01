f19e538661f52f8c1ade8788cd9c02000fa00f5c
Mailtime <mailtime2010@gmail.com>
2018-08-01 17:29:17 +0300

    L11. MultiStream

--- src/com/basejava/webapp/MainThreadCurrency.java
+++ src/com/basejava/webapp/MainThreadCurrency.java
@@ -3,19 +3,22 @@ package com.basejava.webapp;
 public class MainThreadCurrency {
     public static void main(String[] args) {
         System.out.println(Thread.currentThread().getName());
-        new Thread(){
+        Thread thread0 = new Thread() {
             @Override
             public void run() {
-                System.out.println(getName());
+                System.out.println(getName() + " " + getState());
             }
-        }.start();
+        };
+        thread0.start();
         //start second way Thread is better
         new Thread(() -> {
             //don't work cause the cod inside Runnable(have not method getName) not in Thread
 //                System.out.println(getName());
             //Right way. take current Thread and take getName
-            System.out.println(Thread.currentThread().getName());
+            System.out.println(Thread.currentThread().getName() +
+                    " " + Thread.currentThread().getState());
 
         }).start();//lambda
+        System.out.println(thread0.getState());
     }
 }

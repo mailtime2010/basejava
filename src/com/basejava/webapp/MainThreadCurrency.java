f3556f449574104c52195309cccdd016c8801136
Mailtime <mailtime2010@gmail.com>
2018-08-01 17:22:54 +0300

    L11. MultiStream

--- src/com/basejava/webapp/MainThreadCurrency.java
+++ src/com/basejava/webapp/MainThreadCurrency.java
@@ -10,15 +10,12 @@ public class MainThreadCurrency {
             }
         }.start();
         //start second way Thread is better
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                //don't work cause the cod inside Runnable(have not method getName) not in Thread
+        new Thread(() -> {
+            //don't work cause the cod inside Runnable(have not method getName) not in Thread
 //                System.out.println(getName());
-                //Right way. take current Thread and take getName
-                System.out.println(Thread.currentThread().getName());
+            //Right way. take current Thread and take getName
+            System.out.println(Thread.currentThread().getName());
 
-            }
-        }).start();
+        }).start();//lambda
     }
 }

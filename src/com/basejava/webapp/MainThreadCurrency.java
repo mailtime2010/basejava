64f2d8a02ee66289e6486a7fae904c46a82f9218
Mailtime <mailtime2010@gmail.com>
2018-08-01 17:09:21 +0300

    L11. MultiStream

--- src/com/basejava/webapp/MainThreadCurrency.java
+++ src/com/basejava/webapp/MainThreadCurrency.java
@@ -1,6 +1,6 @@
 package com.basejava.webapp;
 
-public class MainCorrency {
+public class MainThreadCurrency {
     public static void main(String[] args) {
         System.out.println(Thread.currentThread().getName());
         new Thread(){

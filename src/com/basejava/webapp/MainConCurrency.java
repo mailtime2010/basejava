a87cf4cec430da89c9e752f5aef4ad3ca0aa2973
Mailtime <mailtime2010@gmail.com>
2018-08-01 15:37:38 +0300

    HMW11. MultiStream

--- src/com/basejava/webapp/MainConCurrency.java
+++ src/com/basejava/webapp/MainConCurrency.java
@@ -6,63 +6,67 @@ import java.util.List;
 public class MainConCurrency {
     private static final int THREADS_NUMBER = 10000;
     private int counter;
-    //   private static final Object LOCK = new Object();
+    private static final Object LOCK = new Object();
 
-    public static void main(String[] args) throws IllegalStateException {
-
-        System.out.println(Thread.currentThread().getName() + " " + Thread.currentThread().getState());
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() +" "+Thread.currentThread().getState());
 
         Thread thread0 = new Thread() {
             @Override
             public void run() {
-                System.out.println(getName() + " " + getState());
-                //    throw new IllegalStateException();
+                System.out.println(getName() + ", " + getState());
+                throw new IllegalStateException();
             }
         };
         thread0.start();
 
-      /*          private void inc() {
-                      synchronized(this) {
-      //                counter++;
-                      }
-                  }*/
-        new Thread(() -> System.out.println(Thread.currentThread().getName() + " " + Thread.currentThread().getState())).start();
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
+            }
+
+            private void inc() {
+                synchronized (this) {
+//                    counter++;
+                }
+            }
+
+        }).start();
 
         System.out.println(thread0.getState());
-    //    System.out.println(Thread.currentThread().getName() + " " + Thread.currentThread().getState());
 
-
-        final MainConCurrency mainConCurrency = new MainConCurrency();
+        final MainConCurrency mainConcurrency = new MainConCurrency();
         List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
 
-        for(int i = 0; i < THREADS_NUMBER; i++) {
+        for (int i = 0; i < THREADS_NUMBER; i++) {
             Thread thread = new Thread(() -> {
-                for(int j = 0; j < 100; j++) {
-                    mainConCurrency.inc();
+                for (int j = 0; j < 100; j++) {
+                    mainConcurrency.inc();
                 }
             });
             thread.start();
             threads.add(thread);
         }
+
         threads.forEach(t -> {
             try {
                 t.join();
-            } catch(InterruptedException e) {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         });
-        System.out.println(mainConCurrency.counter);
+        System.out.println(mainConcurrency.counter);
     }
 
     private synchronized void inc() {
-//        synchronized(this){
-//            synchronized(MainConCurrency.class){
+//        synchronized (this) {
+//        synchronized (MainConcurrency.class) {
         counter++;
 //                wait();
 //                readFile
-//                        ....
-//            }
+//                ...
+//        }
     }
 }
-
-

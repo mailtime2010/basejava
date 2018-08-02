67b6a1f64ddb9093aef752b8fbbe9a44623ffb0d
Mailtime <mailtime2010@gmail.com>
2018-08-02 17:36:40 +0300

    L11. MultiStream(func synchronized)

--- src/com/basejava/webapp/MainThreadCurrency.java
+++ src/com/basejava/webapp/MainThreadCurrency.java
@@ -2,7 +2,7 @@ package com.basejava.webapp;
 
 public class MainThreadCurrency {
     private static int counter;
-    public static final Object LOCK = new Object();
+    private static final Object LOCK = new Object();
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
@@ -24,26 +24,37 @@ public class MainThreadCurrency {
         }).start();//lambda
         System.out.println(thread0.getState());
         // out 1 000 000 in one thread app
+        final MainThreadCurrency mainConCurrency = new MainThreadCurrency();
         for(int i = 0; i < 10000; i++) {
             new Thread(() -> {// create 10 000 threads(but.. 999468)cause: all threads works with counter
                 for(int j = 0; j < 100; j++) {
-                    inc();//make one queue
+                    mainConCurrency.inc();//make one queue
                 }
             }).start();
         }
         Thread.sleep(500);
         System.out.println(counter);
     }
+
     //synchronized only one thread can enter in inc(99990)cause: threads work after sout(counter)
     //full function synchronized
    /* private static synchronized void inc() {
         counter++;
     }*/
-   //the part of function have synchronized - 988106
-   private static void inc(){
-       double d = Math.cos(25.);
-       synchronized(LOCK){
-           counter++;
-       }
-   }
+    //the part of function have synchronized - 988106
+    //static func
+   /* private static void inc() {
+        double d = Math.cos(25.);
+        synchronized(LOCK) {
+            counter++;
+        }*/
+
+    private synchronized void inc() {
+        //synchronized(this){
+        //synchronized(MainConCurrency.class) {
+        double d = Math.cos(24.);
+        //synchronized(LOCK) {
+        counter++;
+        // }
+    }
 }

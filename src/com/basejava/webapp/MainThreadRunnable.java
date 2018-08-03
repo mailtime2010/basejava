3f54cdc06af7d25d1bdebbb5a6113108cd9845d4
Mailtime <mailtime2010@gmail.com>
2018-08-03 14:44:22 +0300

    L11. MultiStream(book Runnable static class)

--- src/com/basejava/webapp/MainThreadRunnable.java
+++ src/com/basejava/webapp/MainThreadRunnable.java
@@ -14,29 +14,28 @@ public class MainThreadRunnable {
             System.out.println("Главный поток прерван.");
         }
         System.out.println("Главный поток завершен.");
-
     }
-}
+    static class NewThread implements Runnable {
+        Thread t;
 
-class NewThread implements Runnable {
-    Thread t;
-
-    NewThread() {
-        t = new Thread(this, "Демонстрационный поток");
-        System.out.println("Дочерний поток создан: " + t);
-        t.start();
-    }
-
-    @Override
-    public void run() {
-        try {
-            for(int i = 5; i > 0; i--) {
-                System.out.println("Дочерний поток: " + i);
-                Thread.sleep(500);
-            }
-        } catch(InterruptedException e) {
-            System.out.println("Дочерний поток прерван.");
+        NewThread() {
+            t = new Thread(this, "Демонстрационный поток");
+            System.out.println("Дочерний поток создан: " + t);
+            t.start();
         }
-        System.out.println("Дочерний поток завершен.");
+
+        @Override
+        public void run() {
+            try {
+                for(int i = 5; i > 0; i--) {
+                    System.out.println("Дочерний поток: " + i);
+                    Thread.sleep(500);
+                }
+            } catch(InterruptedException e) {
+                System.out.println("Дочерний поток прерван.");
+            }
+            System.out.println("Дочерний поток завершен.");
+        }
     }
 }
+

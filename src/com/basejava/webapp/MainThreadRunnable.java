eb8cbf4291e73b983523667bd7a80ac6e04f03f7
Mailtime <mailtime2010@gmail.com>
2018-08-03 13:58:23 +0300

    L11. MultiStream(book Runnable)

--- src/com/basejava/webapp/MainThreadRunnable.java
+++ src/com/basejava/webapp/MainThreadRunnable.java
@@ -4,6 +4,16 @@ public class MainThreadRunnable{
     public static void main(String[] args) {
         new NewThread();
 
+        try {
+            for (int i = 5; i >0 ; i--){
+                System.out.println("Главный поток: " + i);
+                Thread.sleep(1000);
+            }
+        } catch(InterruptedException e) {
+            System.out.println("Главный поток прерван.");
+        }
+        System.out.println("Главный поток завершен.");
+
     }
 }
 class NewThread implements Runnable {

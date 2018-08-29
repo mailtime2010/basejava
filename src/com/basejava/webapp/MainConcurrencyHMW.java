2e02c672aa786d17c722cc707a3c3f72eea03a47
Mailtime <mailtime2010@gmail.com>
2018-08-29 06:28:49 -0400

    HW_10 ConCurrency_12

---
+++ src/com/basejava/webapp/MainConcurrencyHMW.java
@@ -0,0 +1,110 @@
+package com.basejava.webapp;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MainConcurrencyHMW {
+    private static final int THREADS_NUMBER = 10000;
+    private int counter;
+    private final AtomicInteger atomicCounter = new AtomicInteger();
+
+   // private static final ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
+  //  private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+  //  private static final ThreadLocal threadLocal = ThreadLocal.withInitial(() -> new SimpleDateFormat("dd.MM.yyyy HH:mm:ss"));
+
+  /*  private static final ThreadLocal<SimpleDateFormat> threadLocal = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        }
+    };*/
+    private static final ThreadLocal<SimpleDateFormat> threadLocal =
+          ThreadLocal.withInitial(() -> new SimpleDateFormat("dd.MM.yyyy HH:mm:ss"));
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName());
+
+        Thread thread0 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(getName() + ", " + getState());
+//                throw new IllegalStateException();
+            }
+        };
+        thread0.start();
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
+            }
+
+            private void inc() {
+                synchronized (this) {
+//                    counter++;
+                }
+            }
+
+        }).start();
+
+        System.out.println(thread0.getState());
+
+        final MainConcurrencyHMW mainConcurrencyHMW = new MainConcurrencyHMW();
+        CountDownLatch latch = new CountDownLatch(THREADS_NUMBER);
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+//      CompletionService completionService = new ExecutorCompletionService(executorService);
+//
+//      List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+
+        for (int i = 0; i < THREADS_NUMBER; i++) {
+
+            Future<Integer> future = executorService.submit(() ->
+//            Thread thread = new Thread(() ->
+            {
+                for (int j = 0; j < 100; j++) {
+                    mainConcurrencyHMW.inc();
+                    System.out.println(threadLocal.get().format(new Date()));
+                }
+                latch.countDown();
+                return 5;
+            });
+//            thread.start();
+//            threads.add(thread);
+        }
+
+/*
+        threads.forEach(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+*/
+        latch.await(10, TimeUnit.SECONDS);
+        executorService.shutdown();
+//        System.out.println(mainConcurrencyHMW.counter);
+        System.out.println(mainConcurrencyHMW.atomicCounter.get());
+
+
+    }
+
+    private void inc() {
+//        synchronized (this) {
+//        synchronized (MainConcurrency.class) {
+//        WRITE_LOCK.lock();
+//        try {
+        atomicCounter.incrementAndGet();
+//            counter++;
+//        } finally {
+//            WRITE_LOCK.unlock();
+//        }
+//                wait();
+//                readFile
+//                ...
+//        }
+    }
+}

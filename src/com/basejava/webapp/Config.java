819ddd43c6615357e1cef12cedddf727323c1b25
Mailtime <mailtime2010@gmail.com>
2018-09-03 09:03:27 -0400

    L13 DB_config

--- src/com/basejava/webapp/Config.java
+++ src/com/basejava/webapp/Config.java
@@ -7,8 +7,9 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
+    private static final File PROPS = new File("config/resumes.properties");
     private static final Config INSTANCE = new Config();
-    private static final File PROPS = new File("config\\resumes.properties");
+
     private Properties properties = new Properties();
     private File storageDir;
 

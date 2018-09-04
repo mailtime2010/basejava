da3241163cba0c18ae523ca1515c3ebed49e4f63
Mailtime <mailtime2010@gmail.com>
2018-09-04 04:53:25 -0400

    L13 DB_config

--- src/com/basejava/webapp/Config.java
+++ src/com/basejava/webapp/Config.java
@@ -10,7 +10,6 @@ public class Config {
     private static final File PROPS = new File("config/resumes.properties");
     private static final Config INSTANCE = new Config();
 
-    private Properties properties = new Properties();
     private File storageDir;
 
     public static Config getInstance() {
@@ -19,6 +18,7 @@ public class Config {
 
     private Config() {
         try (InputStream is = new FileInputStream(PROPS)) {
+            Properties properties = new Properties();
             properties.load(is);
             storageDir = new File(properties.getProperty("storage.dir"));
         } catch (IOException e) {

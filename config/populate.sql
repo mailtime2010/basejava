97eb05be60fb2100f362852469b42efa172a8bce
Mailtime <mailtime2010@gmail.com>
2018-09-04 07:43:07 -0400

    HW13 DB_SQL

---
+++ config/populate.sql
@@ -0,0 +1,4 @@
+INSERT INTO resume(uuid, full_name) VALUES
+('be860d4d-8939-4fe9-b626-dd58349a3790','Name1'),
+('0aaa1290-cc04-40ef-a3e9-37166739fc1a','Name2'),
+('e0abaf81-8888-4dfa-8e71-84b65a2f20b7','Name3');
